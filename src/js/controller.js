696f0aac9ee330dd729b73fe937203496efb431a
Paulo Henrique de Meneses Oliveira <ph.1928@hotmail.com>
2024-05-28 22:39:11 -0300

    First commit
    add first api call
    lesson 289

---
+++ src/js/controller.js
@@ -0,0 +1,45 @@
+const recipeContainer = document.querySelector('.recipe');
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+const showRecipe = async function() {
+  try {
+    // const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/664c8f193e7aa067e94e8706');
+    const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/664c8f193e7aa067e94e84c1');
+    const data = await res.json();
+    
+    console.log(data);
+    let {recipe} = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients
+    };
+
+    console.log(recipe);
+
+    if (!res.ok) {
+      throw new Error(`${data.message} (${res.status})`)
+    }
+  } catch (err) {
+    alert(err);
+  }
+}
+
+showRecipe();
