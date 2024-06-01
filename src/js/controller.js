1fbab623afd8e36a1d70cb2ceadea23eaf380749
Paulo Henrique de Meneses Oliveira <ph.1928@hotmail.com>
2024-06-01 11:42:28 -0300

    Add search function

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,9 +1,14 @@
 import * as model from './model.js'
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+
+if (module.hot) {
+  module.hot.accept();
+}
 
 // https://forkify-api.herokuapp.com/v2
 
@@ -19,7 +24,6 @@ const controlRecipes = async function() {
 
     // Rendering recipe
     recipeView.render(model.state.recipe);
-
   } catch (err) {
     console.log(err);
     recipeView.renderError();
@@ -28,6 +32,8 @@ const controlRecipes = async function() {
 
 const controlSearchResults = async function() {
   try {
+    resultsView.renderSpinner();
+
     // Get search query
     const query = searchView.getQuery();
     if (!query) return;
@@ -36,7 +42,7 @@ const controlSearchResults = async function() {
     await model.loadSearchResults(query);
     
     // Render results
-    console.log(model.state.search);
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err, '!!!!!!!!!!!');
   }
@@ -47,5 +53,3 @@ const init = function() {
   searchView.addHandlerSearch(controlSearchResults);
 }
 init();
-
-controlSearchResults();
